3705d2196bbc44d8e92b84ef2d5a07e0b47fe76a
Luis O <humbledore@gmail.com>
2018-04-03 12:51:47 -0400

    Creating feature of adding a new user. Interacting with views, controller and model of hobbyist.

--- main.py
+++ main.py
@@ -9,9 +9,95 @@ app.secret_key = 'super-secret-close-your-eyes'
 @app.route('/', methods=['POST','GET'])
 def index():
     hobbyists = Hobbyist.query.all()
-    return render_template('index.html',title="Blogz", hobbyists=hobbyists)
+    return render_template('index.html',title="Hobby Pack", hobbyists=hobbyists)
 
 endpoints_without_login = ['login', 'signup', 'index', 'listing_blogs']
+
+@app.route("/newhobbyist", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        hobbyistname = request.form['hobbyistnamesignup']
+        password = request.form['passwordsignup']
+        verify_password = request.form['verifysignup']
+        email = request.form['emailaddresssignup']
+        city = request.form['citysignup']
+        zipcode = request.form['zipsignup']
+        #Validation for all fields not to be empty
+        if ((password =="") or (verify_password=="") or (hobbyistname=="") or (email=="") or (city=="") or (zipcode=="")):
+            error_empty = "One or more fields are invalid. Please do not leave any field empty." 
+        else:
+            error_empty = ""
+        #Validation for username (length at least 3 and maximum 20)
+        if ((len(hobbyistname) < 3) or (len(hobbyistname) > 20)):
+            error_name = 'The nickname entered is invalid. It has to be at least 3 characters long and maximum 20 characters long.'
+        elif (" " in hobbyistname):
+            error_name = 'The nickname entered is invalid. It can not have a space.'
+        else: 
+            error_name = ""
+        #Validation for Password (length at least 3 and maximum 20)
+        if ((len(password) < 3) and (len(password) > 0)):
+            error_password = 'The password entered is invalid. It has to be at least 3 characters long.'
+        elif password != verify_password:
+            error_password = 'Passwords do not match.'
+        else:
+            error_password = ""
+        #Validation for city (length at least 4 in US)
+        if ((len(city) < 4) and (len(city) > 0)):
+            error_city = 'The city entered is invalid. It has to be at least 4 characters long.'    
+        else:
+            error_city = ""     
+        #Validation for email (length 5 in US)
+        if ((len(zipcode) != 5) and (len(zipcode) > 0)):
+            error_zip = 'The zip code entered is invalid. It has to be 5 characters long.'
+        else:
+            error_zip = ""
+        #validation for email
+        if (email != ""):
+            if ((len(email) < 3) and (len(email) > 0)):
+                error_email = 'The email address entered is invalid. It has to be at least 3 characters long.'            
+            elif ((len(email) > 40)):
+                error_email = 'The email address entered is invalid. It has to be 20 characters long maximum.'            
+            elif ((" " in email) or ("@" not in email) or ("." not in email)):
+                error_email = 'The email address entered is invalid.'
+            else:
+                error_email = ""
+        else:
+            error_email = ""
+        #Final validation - Validation pre-database (checking all the data fields are valid)
+        if ((error_empty != "") or (error_name != "") or (error_password != "") or (error_city != "") or (error_zip != "") or (error_email != "")):
+            return render_template('newhobbyist.html', nickname=hobbyistname, email=email, city=city, zipcode=zipcode, errorname=error_name, errorpassword=error_password, erroremail=error_email, errorcity=error_city, errorzip=error_zip, errorempty=error_empty)
+        else:
+            hobbyists = Hobbyist.query.filter_by(nickname=hobbyistname)
+            #Check if there is an user with this name already in the database
+            if hobbyists.count() == 0:
+                #Check if there is an user with the same email address
+                emails = Hobbyist.query.filter_by(email=email)
+                if emails.count() == 0:
+                    hobbyist = Hobbyist(hobbyistname, email, city, zipcode, password)
+                    db.session.add(hobbyist)
+                    db.session.commit()
+                    session['hobbyist'] = hobbyist.nickname
+                    flash('Logged in. Welcome, ' + str(hobbyist.nickname), 'allgood')
+                    return redirect("/newpost")
+                else:
+                    error_empty = '''The email address "''' + str(email) + '''" already exists. Are you sure you are not signed up already?'''                
+                    return render_template('newhobbyist.html', nickname=hobbyistname, email=email, city=city, zipcode=zipcode, errorempty=error_empty)
+            else:
+                    error_empty = '''The hobbyist name "''' + str(hobbyistname) + '''" already exists. Please signup with another hobbyist name.'''                
+                    return render_template('newhobbyist.html', nickname=hobbyistname, email=email, city=city, zipcode=zipcode, errorempty=error_empty)        
+    else:
+        return render_template('newhobbyist.html')
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def adding_post():
+
+    if request.method == "GET":
+        return render_template('newpost.html')
+
+@app.route('/logout')
+def saliendo():
+    del session['hobbyist']
+    return redirect('/')
 
 @app.before_request
 def require_login():
