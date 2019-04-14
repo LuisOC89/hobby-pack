36f95851566fc91cac18d2adfcfe610e4a611a2a
Luis Humberto Orozco Cabrera <humbledore@gmail.com>
2019-04-14 17:13:40 -0400

    Fixed vulnerability, improved connection string to psql, added config file

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+
+from conf.config import DB_TYPE, DB_DRIVER, DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = '{}+{}://{}:{}@{}:{}/{}'.format(DB_TYPE, DB_DRIVER, DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
 # Connection String To connect to database: app.config['SQLAlchemy_DATABASE_URI'] = 'type_of_database+driver://username:password@server:port-number/name-of-database'
 
 # MySQL
@@ -13,10 +21,5 @@ app.config['DEBUG'] = True
 # app.config OPTION running directly
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://hobby_pack:'+str((os.environ.get("PASS_DB")))+'@localhost:5432/hobby_pack'
 
-#PostgreSQL
 # app.config OPTION running with Docker
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://hobby_pack:'+str((os.environ.get("PASS_DB")))+'@host.docker.internal:5432/hobby_pack'
-
-app.config['SQLALCHEMY_ECHO'] = True
-
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://hobby_pack:'+str((os.environ.get("PASS_DB")))+'@host.docker.internal:5432/hobby_pack'
---
+++ conf/config.py
@@ -0,0 +1,9 @@
+import os
+
+DB_USER = os.environ["DB_USER"]
+DB_PASS = os.environ["DB_PASS"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
+DB_TYPE = os.environ["DB_TYPE"]
+DB_DRIVER = os.environ["DB_DRIVER"]
